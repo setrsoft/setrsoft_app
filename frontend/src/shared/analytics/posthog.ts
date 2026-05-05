ff43cafc5fcc33c446041b9758c6882ec79c5551
Eloi BERLINGER <eloi.berling@protonmail.com>
2026-05-05 17:10:55 +0200

    Update PostHog configuration

--- frontend/src/shared/analytics/posthog.ts
+++ frontend/src/shared/analytics/posthog.ts
@@ -6,6 +6,8 @@ const host = import.meta.env.VITE_PUBLIC_POSTHOG_HOST as string;
 posthog.init(apiKey, {
   api_host: host,
   ui_host: 'https://eu.posthog.com',
+  defaults: '2026-01-30',
+  person_profiles: 'identified_only',
   autocapture: true,
 });
 
